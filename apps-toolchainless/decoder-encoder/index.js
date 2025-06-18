82e56bb84b2cbf10a4edfef92713435dd5b8e1c5
simshadows <contact@simshadows.com>
2025-06-18 19:46:11 +1000

    Improved string decoder/encoder

--- apps-toolchainless/decoder-encoder/index.js
+++ apps-toolchainless/decoder-encoder/index.js
@@ -6,20 +6,91 @@
 
 //const assert = console.assert;
 
+/*
+ * Encoders/Decoders
+ */
+
+function passthrough(s) {
+    return s;
+}
+
+const decoders = {
+    "none": passthrough,
+    "base64": function(s) {
+        return atob(s);
+    },
+    "url": function(s) {
+        // unescape() is a non-standard browser function.
+        // Consider replacing with something better-supported.
+        return unescape(s);
+    },
+    "xml-entities": function(s) {
+        // We first do a quick test to see if this is unsafe
+        const e = document.createElement("textarea");
+        e.innerHTML = "<script>alert('Error: Your browser failed a compatibility test. Please report this bug on GitHub, or email <contact@simshadows.com>.')</script>";
+        e;
+        // I'm actually not entirely sure if that would ever work.
+        // Better to give it a try and be safe than sorry though.
+        e.innerHTML = s;
+        return e.value;
+    },
+}
+
+const encoders = {
+    "none": passthrough,
+    "base64": function(s) {
+        return btoa(s);
+    },
+    "url": function(s) {
+        // escape() is a non-standard browser function.
+        // Consider replacing with something better-supported.
+        return escape(s);
+    },
+    "xml-entities": function(s) {
+        const e = document.createElement("textarea");
+        e.innerText = s;
+        return e.innerHTML;
+    },
+    "calc-string-length": function(s) {
+        return s.length;
+    },
+}
+
+/*
+ * Rendering
+ */
+
 const inputElement = document.querySelector("#input");
 const outputElement = document.querySelector("#output");
-
-function encodeWithEscape(s) {
-    return escape(s);
-}
+const inputDecodeSelect = document.querySelector("#decode-select");
+const outputEncodeSelect = document.querySelector("#encode-select");
+const runButton = document.querySelector("#run-button");
 
 function render() {
-    const inputValue = String(inputElement.value);
-    outputElement.value = encodeWithEscape(inputValue);
+    try {
+        const inputMode = inputDecodeSelect.value;
+        const outputMode = outputEncodeSelect.value;
+
+        const decoder = decoders[inputMode];
+        const encoder = encoders[outputMode];
+
+        const inputValue = String(inputElement.value);
+        const outputValue = String(encoder(String(decoder(inputValue))));
+        outputElement.value = outputValue;
+    } catch (e) {
+        console.error(e);
+        outputElement.value = String(e);
+    }
 }
 
-inputElement.addEventListener("change", render);
-outputElement.addEventListener("change", render);
+[
+    inputElement,
+    //outputElement
+    inputDecodeSelect,
+    outputEncodeSelect,
+].forEach(x => x.addEventListener("change", render));
+
+runButton.addEventListener("click", render);
 
 render();
 
