8ac3b854a1826947ccb3379b5f52caf576f75632
simshadows <contact@simshadows.com>
2025-06-24 01:54:57 +1000

    Improved string decoder/encoder

--- apps-toolchainless/decoder-encoder/index.js
+++ apps-toolchainless/decoder-encoder/index.js
@@ -4,27 +4,31 @@
  * License:  GNU Affero General Public License v3 (AGPL-3.0)
  */
 
-//const assert = console.assert;
-
-/*
- * Encoders/Decoders
- */
-
 const strFunctions = {
     "none": (s) => s,
     "base64-decode": (s) => atob(s),
     "base64-encode": (s) => btoa(s),
-    "regex-literal-encode-jsflavour": (s) => RegExp.escape(s),
-    "url-decode": (s) => {
-        // unescape() is a non-standard browser function.
-        // Consider replacing with something better-supported.
-        return unescape(s);
+
+    "regex-literal-jsflavour-encode": (s) => {
+        return RegExp.escape(s);
     },
-    "url-encode": (s) => {
-        // escape() is a non-standard browser function.
-        // Consider replacing with something better-supported.
-        return escape(s);
-    },
+    // hmm there's an interesting idea here with the modified version.
+    // I'll keep it commented out for now until I've figured out a use for it.
+    //"regex-literal-jsflavour-encode-modified": (s) => {
+    //    const a = RegExp.escape("a");
+    //    const chars = [];
+    //    for (const c of s) {
+    //        const x = RegExp.escape("a" + c);
+    //        chars.push(x);
+    //    }
+    //    return chars.join("");
+    //},
+
+    // escape() and unescape() are non-standard browser functions.
+    // Consider replacing with something better-supported.
+    "url-decode": (s) => unescape(s),
+    "url-encode": (s) => escape(s),
+
     "xml-entities-decode": (s) => {
         // We first do a quick test to see if this is unsafe
         const e = document.createElement("textarea");
@@ -35,17 +39,27 @@ const strFunctions = {
         e.innerHTML = s;
         return e.value;
     },
+
     "xml-entities-encode": (s) => {
         const e = document.createElement("textarea");
         e.innerText = s;
         return e.innerHTML;
     },
+
     "calc-string-length": (s) => s.length,
+
+    "experimental-regex-escape-by-character": (s) => {
+        const chars = [];
+        for (const c of s) {
+            chars.push(RegExp.escape(c));
+        }
+        return chars.join("");
+    },
+    "experimental-exception": () => {
+        null.foobar()
+    },
 }
 
-/*
- * Rendering
- */
 
 const inputElement = document.querySelector("#input");
 const outputElement = document.querySelector("#output");
@@ -58,17 +72,16 @@ function render() {
         const inputValue = String(inputElement.value);
         const outputValue = String(fn(inputValue));
         outputElement.value = outputValue;
+        outputElement.classList.remove("error");
     } catch (e) {
         console.error(e);
         outputElement.value = String(e);
+        outputElement.classList.add("error");
     }
 }
 
-[
-    inputElement,
-    //outputElement
-    fnSelect,
-].forEach(x => x.addEventListener("change", render));
+inputElement.addEventListener("change", render);
+fnSelect.addEventListener("change", render);
 
 runButton.addEventListener("click", render);
 
